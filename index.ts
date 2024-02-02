4065bd0e18f3d9d77b038e4923fa3451d6926bf4
Dammymure <dammymure1@gmail.com>
2024-02-02 22:32:15 +0100

    completed the shopping controller and tested them

--- index.ts
+++ index.ts
@@ -1,31 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import path from 'path'
+import express from 'express'
+import App from './services/ExpressApp'
+import dbConnection from './services/Database'
 
-import { AdminRoute, VandorRoute } from "./routes";
-import { MONGO_URI } from "./config";
+const StartServer = async () => {
 
-const app = express();
+  const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
+  await dbConnection()
 
-app.use("/admin", AdminRoute);
-app.use("/vandor", VandorRoute);
+  await App(app)
 
+  app.listen(8000, () => {
+    console.log("Listening to port 8000")
+  })
 
-mongoose.connect(MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopography:true,
-  // useCreateIndex: true
-}).then(result => {
-  // console.log(result)
-  console.log("DB connected.....")
-}).catch(err => console.log('error'+ err))
+}
 
-app.listen(8000, () => {
-  console.clear();
-  console.log("App is listening on the port 8000");
-});
+StartServer()
