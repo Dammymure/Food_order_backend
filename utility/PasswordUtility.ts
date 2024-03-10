a03a5c8c97ee0d1034d093e70364e0216dd70c89
Dammymure <dammymure1@gmail.com>
2024-03-10 09:29:45 +0100

    utility

---
+++ utility/PasswordUtility.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import { Request } from "express";
+import jwt from "jsonwebtoken";
+import { VandorPayload } from "../src/dto";
+import { APP_SECRET } from "../src/config";
+import { AuthPayload } from "../src/dto/Auth.dto";
+
+export const GenerateSalt = async () => {
+  return await bcrypt.genSalt();
+};
+
+export const GeneratePassword = async (password: string, salt: string) => {
+  return await bcrypt.hash(password, salt);
+};
+
+export const ValidatePassword = async (
+  enteredPassword: string,
+  savedPassword: string,
+  salt: string
+) => {
+  return (await GeneratePassword(enteredPassword, salt)) === savedPassword;
+};
+
+export const GenerateSignature = (payload: AuthPayload) => {
+  return jwt.sign(payload, APP_SECRET, { expiresIn: "1d" });
+};
+
+export const ValidateSignature = async (req: Request) => {
+  const signature = req.get("Authorization");
+
+  if (signature) {
+    const payload = (await jwt.verify(
+      signature.split(" ")[1],
+      APP_SECRET
+    )) as AuthPayload;
+
+    req.user = payload;
+
+    return true;
+  }
+
+  return false;
+};
